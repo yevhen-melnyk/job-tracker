d9afedfdb9a2de8031c77c175c493a1a037ca637
Yevhen Melnyk <yev.melnyk@gmail.com>
2023-10-22 15:59:48 +0200

    Set dev environment variables

--- angularapp/src/services/fetch-service.ts
+++ angularapp/src/services/fetch-service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { JobResponse } from '../models/job.model';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,6 @@ export class FetchService {
 constructor(private http: HttpClient) { }
 
   getJobs(): Observable<JobResponse[]> {
-    return this.http.get<JobResponse[]>('/jobs');
+    return this.http.get<JobResponse[]>(environment.apiUrl + '/jobs');
   }
 }
